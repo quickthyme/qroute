d1b6b157c2afc548cc80d6f8ef940e71908284e5
Quickthyme <quickthyme@mac.com>
2019-02-26 15:55:53 -0600

    Near/Far example
    Signed-off-by: Quickthyme <quickthyme@mac.com>

--- ExampleApp/Presentation/ToDoDetail/ToDoDetailViewController.swift
+++ ExampleApp/Presentation/ToDoDetail/ToDoDetailViewController.swift
@@ -5,12 +5,12 @@ class ToDoDetailViewController: UIViewController {
     var route: QTRoute? = AppRoute.plan.findDescendent(AppRoute.id.ToDoDetail)!
     var router: QTRouting? = AppRoute.rootRouter
 
-    @IBAction func contactUsAction(_ sender: AnyObject) {
+    @IBAction func contactUsNearAction(_ sender: AnyObject) {
         router?.routeSub(AppRoute.id.ContactUs, from: self, completion: nil)
     }
 
-    @IBAction func messageCenterAction(_ sender: AnyObject) {
-        router?.routeTo(AppRoute.id.MessageCenter, from: self, completion: nil)
+    @IBAction func contactUsFarAction(_ sender: AnyObject) {
+        router?.routeTo(AppRoute.id.ContactUs, from: self, completion: nil)
     }
 
     override func viewDidLoad() {
