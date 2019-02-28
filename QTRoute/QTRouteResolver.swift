a730a7d8c05c9e9d568a1864040761bc907879cb
Quickthyme <quickthyme@mac.com>
2019-02-28 17:38:19 -0600

    refactor route resolver
    Signed-off-by: Quickthyme <quickthyme@mac.com>

--- QTRoute/QTRouteResolver.swift
+++ QTRoute/QTRouteResolver.swift
@@ -1,42 +1,43 @@
 
 class QTRouteResolver: QTRouteResolving {
+    typealias ToChild  = (QTRoute, QTRoutable, QTRoutableInput, @escaping QTRoutableCompletion) -> ()
+    typealias ToParent = (QTRoutable, QTRoutableInput, @escaping QTRoutableCompletion) -> ()
+    typealias ToSelf   = (QTRoutable, QTRoutableInput, @escaping QTRoutableCompletion) -> ()
+
     var route: QTRoute
     private let toChild: ToChild
     private let toParent: ToParent
     private let toSelf: ToSelf
 
-    typealias ToChild  = (QTRoute, QTRoutable, QTRoutableInput, @escaping QTRoutableCompletion) -> ()
-    typealias ToParent = (QTRoutable, QTRoutableInput, @escaping QTRoutableCompletion) -> ()
-    typealias ToSelf   = (QTRoutable, QTRoutableInput, @escaping QTRoutableCompletion) -> ()
-
-    convenience init(_ route: QTRoute,
+    init(_ route: QTRoute,
          toChild: @escaping ToChild,
-         toParent: @escaping ToParent) {
-        self.init(route, toChild: toChild, toParent: toParent, toSelf: {
-                from,input,completion in
-                QTRouteResolver.mergeInputDependencies(target: from, input: input)
-                completion(from) }
-        )
-    }
-
-    init(_ route: QTRoute,toChild: @escaping ToChild,
-         toParent: @escaping ToParent, toSelf: @escaping ToSelf) {
+         toParent: @escaping ToParent,
+         toSelf: @escaping ToSelf = defaultToSelf) {
         self.route = route
         self.toChild = toChild
         self.toParent = toParent
         self.toSelf = toSelf
     }
 
-    func resolveRouteToChild(_ route: QTRoute, from: QTRoutable, input: QTRoutableInput, completion: @escaping QTRoutableCompletion) {
+    func resolveRouteToChild(_ route: QTRoute, from: QTRoutable, input: QTRoutableInput,
+                             completion: @escaping QTRoutableCompletion) {
         toChild(route, from, input, completion)
     }
 
-    func resolveRouteToParent(from: QTRoutable, input: QTRoutableInput, completion: @escaping QTRoutableCompletion) {
+    func resolveRouteToParent(from: QTRoutable, input: QTRoutableInput,
+                              completion: @escaping QTRoutableCompletion) {
         toParent(from, input, completion)
     }
 
-    func resolveRouteToSelf(from: QTRoutable, input: QTRoutableInput, completion: @escaping QTRoutableCompletion) {
+    func resolveRouteToSelf(from: QTRoutable, input: QTRoutableInput,
+                            completion: @escaping QTRoutableCompletion) {
         toSelf(from, input, completion)
     }
 
+    private static var defaultToSelf: ToSelf {
+        return { from,input,completion in
+            QTRouteResolver.mergeInputDependencies(target: from, input: input)
+            completion(from)
+        }
+    }
 }
